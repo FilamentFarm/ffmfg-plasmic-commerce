9386e1a61f0cd1b842d09357fb88abd90a626df8
FilamentFarm <filamentfarm@gmail.com>
2025-11-20 15:15:00 -0700

    add /plasmic test page

---
+++ app/plasmic/page.tsx
@@ -0,0 +1,26 @@
+import { notFound } from 'next/navigation';
+import { PLASMIC } from '@/lib/plasmic-init';
+import {
+  PlasmicRootProvider,
+  PlasmicComponent,
+} from '@plasmicapp/loader-nextjs';
+
+export const dynamic = 'force-dynamic';
+
+export default async function PlasmicTestPage() {
+  // This name must match your Plasmic page name.
+  // From your screenshot it looks like "Homepage".
+  const componentName = 'Homepage';
+
+  const plasmicData = await PLASMIC.fetchComponentData(componentName);
+
+  if (!plasmicData) {
+    notFound();
+  }
+
+  return (
+    <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData}>
+      <PlasmicComponent component={componentName} />
+    </PlasmicRootProvider>
+  );
+}
