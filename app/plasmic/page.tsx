41bbc3e10af71c9a22a0384a396f3e8d7437332b
FilamentFarm <filamentfarm@gmail.com>
2025-11-24 18:37:47 -0700

    Use PlasmicHomepage for /plasmic

--- app/plasmic/page.tsx
+++ app/plasmic/page.tsx
@@ -1,28 +1,5 @@
-'use client';
+import PlasmicHomepage from 'components/plasmic-homepage';
 
-import * as React from 'react';
-import {
-  PlasmicRootProvider,
-  PlasmicComponent,
-} from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from 'lib/plasmic-init'; // <-- fixed import
-
-const COMPONENT_NAME = 'Homepage'; // make sure this matches your Plasmic page name
-
-export default function PlasmicTestPage() {
-  const [plasmicData, setPlasmicData] = React.useState<any | null>(null);
-
-  React.useEffect(() => {
-    PLASMIC.fetchComponentData(COMPONENT_NAME).then(setPlasmicData);
-  }, []);
-
-  if (!plasmicData) {
-    return <div style={{ padding: 24 }}>Loading Plasmic page…</div>;
-  }
-
-  return (
-    <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData}>
-      <PlasmicComponent component={COMPONENT_NAME} />
-    </PlasmicRootProvider>
-  );
+export default function PlasmicRoutePage() {
+  return <PlasmicHomepage />;
 }
