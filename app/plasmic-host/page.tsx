a6d10f3cce7f33596bdbd7d1580bdffe157c99e2
FilamentFarm <filamentfarm@gmail.com>
2025-11-27 08:25:34 -0700

    Remove plasmic-host-init import from /plasmic-host

--- app/plasmic-host/page.tsx
+++ app/plasmic-host/page.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from 'lib/plasmic-init';
-import 'lib/plasmic-host-init'; // register Next.js components for Plasmic Studio
 
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
